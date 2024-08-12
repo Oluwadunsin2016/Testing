1e091ec2a018bed21eb922fedbdb7cb7e416e9b8
Oluwadunsin <oluwadunsin2016@gmail.com>
2024-08-12 08:37:24 +0100

    updated

--- clancomdashboard/src/App.jsx
+++ clancomdashboard/src/App.jsx
@@ -10,30 +10,15 @@ const App = () => {
 const [tab, setTab] = useState('Projects')
   return (
     <>
-    {/* <RootLayout>
-      <Routes>
-        <Route path="/" element={<AllApps />} />
-        <Route path="/authentication" element={<Authentication />} />
-        <Route path="/Enterprise/storage" element={<Stroage />} />
-        <Route path="/Enterprise/app-settings" element={<Settings />} />
-
-      </Routes>
-    </RootLayout> */}
     <div>
 <div  className="p-8">
-{/* <div  className="flex flex-wrap "> */}
 <div  className="grid grid-cols-1 md:grid-cols-4 gap-8">
-    {/* <div  className="xl:w-2/3 pr-4 pl-4 flex flex-col gap-6"> */}
     <div  className="md:col-span-3 flex flex-col gap-6">
       <Profile setTab={setTab} tab={tab} />
-        {/* <!-- end card --> */}
 
     <DisplayArea tab={tab}/>
-        {/* <!-- end card --> */}
     </div>
-    {/* <!-- end col --> */}
    <SideContents/>
-    {/* <!-- end col --> */}
 </div>
 </div>
     </div>
